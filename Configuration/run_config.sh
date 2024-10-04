97a19aa66cb440baf8d59d3decc9cf259ee81eae
André Sacilotto Santos <andre.santos01@edu.pucrs.br>
2024-10-03 21:08:39 -0300

    feat: Everyhting ok

--- Configuration/run_config.sh
+++ Configuration/run_config.sh
@@ -7,5 +7,3 @@ ansible-playbook -i inventory.ini ./runners.yml
 ansible-playbook -i inventory.ini ./database.yml
 
 ansible-playbook -i inventory.ini ./backend.yml
-
-ansible-playbook -i inventory.ini ./frontend.yml
