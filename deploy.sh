ea894ab5ec06cb132c1174a4c55bcc0e20a6c61b
André Sacilotto Santos <andre.santos01@edu.pucrs.br>
2024-10-07 02:42:33 -0300

    fix: uncommented

--- deploy.sh
+++ deploy.sh
@@ -29,10 +29,11 @@ ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i ./static_ip.ini ./swarm_setup.y
 # sleep 5
 
 cd ..
-#  cd Configuration/
 
-#  Navigate to Ansible directory and run playbook
-#  echo "Running Ansible runners playbook..."
-# ./run_config.sh
+cd Configuration/
+
+ Navigate to Ansible directory and run playbook
+ echo "Running Ansible runners playbook..."
+./run_config.sh
 
 echo "Deployment complete."
