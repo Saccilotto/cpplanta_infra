cdbea4d495f0aa8fda9736f6c267383d8556b98f
André Sacilotto Santos <andre.santos01@edu.pucrs.br>
2024-10-11 00:43:51 -0300

    fix: uncommented now needed scripts/features

--- deploy.sh
+++ deploy.sh
@@ -5,35 +5,19 @@ set -e
 # Set project vars
 export $(grep -v '^#' .env | xargs)
 
-# # Navigate to Terraform directory and initialize Terraform
-# echo "Initializing Terraform..."
-# cd Terraform/
-# terraform init
-
-# # Apply Terraform configuration
-# echo "Applying Terraform configuration..."
-# terraform apply -auto-approve
-
-# cd ..
-
-# Sleep for instances to boot up
-# # echo "Waiting for instances to boot up..."
-# sleep 7
-
 cd Swarm/
 
 # Deploy do stack no Docker Swarm
 echo "Deploying Docker Swarm stack..."
 ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i ../static_ip.ini ./swarm_setup.yml
 
-# sleep 5
+sleep 5
 
+cd ../Configuration/
 
-# cd ../Configuration/
-
-# # Navigate to Ansible directory and run playbook
-# echo "Running Ansible runners playbook..."
-# ./run_config.sh
+# Navigate to Ansible directory and run playbook
+echo "Running Ansible runners playbook..."
+./run_config.sh
 
 cd ..
 echo "Deployment complete."
